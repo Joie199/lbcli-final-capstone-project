af60b49dd4b6b0152e49391fcce2670aea8fd257
joie199 <joieama2@gmail.com>
2026-04-23 00:00:24 +0300

    Update 05.sh

--- submission/05.sh
+++ submission/05.sh
@@ -1,8 +1,20 @@
 # How many satoshis did this transaction pay for fee?: b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb
 txid="b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb"
 
-in=$(bitcoin-cli -signet getrawtransaction "$txid" true | jq '[.vin[].txid] | unique | .[]' | xargs -I {} bitcoin-cli getrawtransaction {} true | jq '[.vout[].value] | add')
+in_sum=0
 
-out=$(bitcoin-cli -signet getrawtransaction "$txid" true | jq '[.vout[].value] | add')
+for row in $(bitcoin-cli -signet getrawtransaction "$txid" true | jq -r '.vin[] | "\(.txid):\(.vout)"')
+do
+    prev_txid=$(echo "$row" | cut -d: -f1)
+    vout_index=$(echo "$row" | cut -d: -f2)
 
-echo "($in - $out) * 100000000"
+    value=$(bitcoin-cli -signet getrawtransaction "$prev_txid" true \
+    | jq -r ".vout[$vout_index].value")
+
+    in_sum=$(echo "$in_sum + $value" | bc)
+done
+
+out_sum=$(bitcoin-cli -signet getrawtransaction "$txid" true \
+| jq '[.vout[].value] | add')
+
+echo "($in_sum - $out_sum) * 100000000" | bc
