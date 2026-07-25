4a685cfdcb3b74eaaadb920469dc427f49d33b4f
joie199 <joieama2@gmail.com>
2026-07-25 23:54:59 +0300

    Update 05.sh

--- submission/05.sh
+++ submission/05.sh
@@ -1,3 +1,12 @@
 # How many satoshis did this transaction pay for fee?: b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb
 txid="b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb"
-bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getrawtransaction "$txid" true | jq '[.vin[] | .txid + ":" + (.vout|tostring)]'
+
+
+prevtx=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect="167.172.185.136" -rpcport=38332 getrawtransaction $txid true| jq -r '.vin[0].txid')
+vout=$( bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect="167.172.185.136" -rpcport=38332 getrawtransaction $txid true | jq -r '.vin[0].vout')
+
+inputs=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect="167.172.185.136" -rpcport=38332 getrawtransaction $prevtx true | jq ".vout[$vout].value")
+
+outputs=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect="167.172.185.136" -rpcport=38332 getrawtransaction $txid true | jq '[.vout[].value] | add')
+
+echo "($inputs - $outputs) * 100000000" | bc
