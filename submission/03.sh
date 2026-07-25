3616be26005b3175f6b4a5bbeb6e40449b3f2d70
joie199 <joieama2@gmail.com>
2026-07-25 22:48:12 +0300

    update

--- submission/03.sh
+++ submission/03.sh
@@ -4,8 +4,8 @@ block_hash=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustb
 
 block_hash2=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblockhash 216351)
 
-coinbase_txid=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblock "$block_hash" | jq -r '.tx[0]')
+coinbase_txid=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblock "$block_hash" 1 | jq -r '.tx[0]')
 
-spending_txid=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblock "$block_hash2" 2 | jq -r --arg txid "$coinbase_txid" '.tx[] | select(.vin[]?.txid == $txid) | .txid')
+spending_txid=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblock "$block_hash2" 1 | jq -r --arg txid "$coinbase_txid" '.tx[] | select(.vin[]?.txid == $txid) | .txid')
 
 echo "$spending_txid"
