680cdadf6a3f3c05066f26f590d87656b530e980
joie199 <joieama2@gmail.com>
2026-07-25 12:19:39 +0300

    Update 08.sh

--- submission/08.sh
+++ submission/08.sh
@@ -2,4 +2,4 @@
 txid="49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb"
 
 blockheight=$(bitcoin-cli gettransaction $txid | jq -r '.blockheight')
-echo "$blockheight"
+echo "$blockheight" 
