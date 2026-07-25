8e7b860ee5db9c76a7e8413c7ee6d8c844de30ad
joie199 <joieama2@gmail.com>
2026-07-25 23:24:53 +0300

    Update 05.sh

--- submission/05.sh
+++ submission/05.sh
@@ -1,4 +1,3 @@
 # How many satoshis did this transaction pay for fee?: b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb
-txid=b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb
-fee=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 gettransaction $txid | jq -r '.fee')
-echo "$fee"
+txid="b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb"
+ bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getrawtransaction "$txid" true | jq '.fee * 100000000 | floor'
