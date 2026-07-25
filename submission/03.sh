279d233beef4ba5fae051317a9304c74ecc248a6
joie199 <joieama2@gmail.com>
2026-07-25 22:58:08 +0300

    update

--- submission/03.sh
+++ submission/03.sh
@@ -6,6 +6,6 @@ block_hash2=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrust
 
 coinbase_txid=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblock "$block_hash" 1 | jq -r '.tx[0]')
 
-spending_txid=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblock "$block_hash2" 1 | jq -r --arg txid "$coinbase_txid" '.tx[] | select(.vin[]?.txid == $txid) | .txid')
+spending_txid=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblock "$block_hash2" 2 | jq -r --arg txid "$coinbase_txid" '.tx[] | select(.vin[]?.txid == $txid) | .txid')
 
 echo "$spending_txid"
