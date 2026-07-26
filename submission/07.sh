ce65d8fe1d6af0918976f05a7770328fd4611a69
joie199 <joieama2@gmail.com>
2026-07-27 01:22:24 +0300

    update 08

--- submission/07.sh
+++ submission/07.sh
@@ -1,4 +1,5 @@
 # what is the coinbase tx in this block 243,834
-hash=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblockhash 243834)
-coinbase_txid=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblock $hash | jq -r '.tx[0]')
+RPC="bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332"
+hash=$($RPC getblockhash 243834)
+coinbase_txid=$($RPC getblock $hash | jq -r '.tx[0]')
 echo "$coinbase_txid"
