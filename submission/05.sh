a45aea809b7f00d05f17d42d8e07767914a86ecd
joie199 <joieama2@gmail.com>
2026-07-26 10:29:46 +0300

    Update 05.sh

--- submission/05.sh
+++ submission/05.sh
@@ -1,13 +1,15 @@
 # How many satoshis did this transaction pay for fee?: b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb
 txid="b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb"
 
-tx=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect="167.172.185.136" -rpcport=38332 getrawtransaction $txid true)
+tx=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect="167.172.185.136" -rpcport=38332 getrawtransaction "$txid" true)
 
 prevtx=$(echo "$tx" | jq -r '.vin[0].txid')
 vout=$(echo "$tx" | jq -r '.vin[0].vout')
 
-inputs=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect="167.172.185.136" -rpcport=38332 getrawtransaction $prevtx true | jq ".vout[$vout].value")
+inputs=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect="167.172.185.136" -rpcport=38332 getrawtransaction "$prevtx" true | jq ".vout[$vout].value")
 
 outputs=$(echo "$tx" | jq '[.vout[].value] | add')
 
-echo "($inputs - $outputs) * 100000000"
+fee=$(awk "BEGIN {printf \"%.0f\", ($inputs - $outputs) * 100000000}")
+
+echo "$fee"
